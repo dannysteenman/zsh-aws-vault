3d911a25c46c6ef35fbad606f50655e06f84d229
Danny Steenman <danny.steenman@icloud.com>
2022-01-24 09:14:38 +0100

    feat: improve functions

--- aws-vault.plugin.zsh
+++ aws-vault.plugin.zsh
@@ -11,6 +11,8 @@ alias ave='aws-vault exec'
 alias avl='aws-vault login'
 alias avll='aws-vault login -s'
 alias avs='aws-vault server'
+
+# aliases for functions
 alias avc='aws-vault-chrome'
 alias avp='aws-vault-profile'
 
@@ -18,53 +20,40 @@ alias avp='aws-vault-profile'
 # Functions
 #------------------
 function aws-vault-profile() {
+  local aws_profile=${1:-$AWS_VAULT}
   if [[ -z "$1" ]]; then
-    unset AWS_SDK_LOAD_CONFIG
-    unset AWS_DEFAULT_PROFILE AWS_PROFILE AWS_EB_PROFILE
-    unset AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY AWS_SESSION_TOKEN AWS_SESSION_EXPIRATION
+    unset AWS_VAULT AWS_SDK_LOAD_CONFIG
+    unset AWS_REGION AWS_DEFAULT_REGION
+    unset AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY
+    unset AWS_PROFILE AWS_DEFAULT_PROFILE AWS_EB_PROFILE
+    unset AWS_SESSION_EXPIRATION AWS_SESSION_TOKEN AWS_SECURITY_TOKEN
     echo AWS Profile cleared.
     return
+  else
+    export AWS_SDK_LOAD_CONFIG=true
+    export AWS_PROFILE=$aws_profile
+    export AWS_DEFAULT_PROFILE=$aws_profile
+    export AWS_EB_PROFILE=$aws_profile
+    export $(aws-vault exec $aws_profile -- env | grep AWS)
+    echo "Switched to AWS Profile: ${aws_profile}"
   fi
-
-  export AWS_SDK_LOAD_CONFIG=true
-  export AWS_DEFAULT_PROFILE=$1
-  export AWS_PROFILE=$1
-  export AWS_EB_PROFILE=$1
-
-  creds=$(mktemp -d)/creds.json
-  aws-vault exec ${AWS_PROFILE} -- env | grep AWS >$creds
-  export AWS_ACCESS_KEY_ID=$(cat ${creds} | grep "AWS_ACCESS_KEY_ID" | cut -d '=' -f 2)
-  export AWS_SECRET_ACCESS_KEY=$(cat ${creds} | grep "AWS_SECRET_ACCESS_KEY" | cut -d '=' -f 2)
-  export AWS_SESSION_TOKEN=$(cat ${creds} | grep "AWS_SESSION_TOKEN" | cut -d '=' -f 2)
-  export AWS_SESSION_EXPIRATION=$(cat ${creds} | grep "AWS_SESSION_EXPIRATION" | cut -d '=' -f 2)
-  echo "Switched to AWS Profile: ${AWS_PROFILE}"
 }
 
 function aws-vault-chrome() {
-  # set to yes to create one-time use profiles in /tmp
-  # anything else will create them in $HOME/.aws/awschrome
-  TEMP_PROFILE="yes"
+  local aws_profile=${1:-$AWS_VAULT}
 
-  aws_profile="$1"
   if [[ -z "$aws_profile" ]]; then
-    echo "aws_profile is a required argument" >&2
+    echo 'Please add an AWS Profile name e.g. avc <aws_profile>' >&2
     return 1
   fi
 
-  # replace non word and not - with __
-  profile_dir_name=${aws_profile//[^a-zA-Z0-9_-]/__}
-  user_data_dir="${HOME}/.aws/awschrome/${profile_dir_name}"
-  extension_dir="~/.aws/awschrome/extensions/i-dont-care-about-cookies"
-
-  if [[ "$TEMP_PROFILE" = "yes" ]]; then
-    user_data_dir=$(mktemp -d /tmp/awschrome_userdata.XXXXXXXX)
-  fi
-
-  url=$(aws-vault login $aws_profile --stdout)
   aws_status=$?
+  url=$(aws-vault login $aws_profile --stdout)
+  profile_dir_name=${aws_profile//[^a-zA-Z0-9_-]/__}
+  user_data_dir=$(mktemp -d /tmp/awschrome_userdata.XXXXXXXX)
+  extension_dir="${HOME}/.aws/awschrome/extensions/i-dont-care-about-cookies"
 
   if [[ ${aws_status} -ne 0 ]]; then
-    # zsh will also capture stderr, so echo $url
     echo ${url}
     return ${aws_status}
   fi
